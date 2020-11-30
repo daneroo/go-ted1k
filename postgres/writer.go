7fe83a1bb139eaa427d6d7d8167841e81ca6ea76
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-11-30 19:02:36 +0000

    Refactor postgres.Write fallback conditions

--- postgres/writer.go
+++ postgres/writer.go
@@ -35,6 +35,9 @@ func NewWriter(conn *pgx.Conn, tableName string) *Writer {
 }
 
 // Write consumes an Entry channel - returns (count,error)
+// The returned count is the number of entries that were processed.
+// Note: We could return the affected rows by summing the returned values from w.flush()
+// Note: We could terminate early in case of error (from w.flush), which we currently silently ignore
 func (w *Writer) Write(src <-chan []types.Entry) (int, error) {
 	count := 0
 	entries := make([]types.Entry, 0, writeBatchSize)
@@ -57,19 +60,46 @@ func (w *Writer) Write(src <-chan []types.Entry) (int, error) {
 	return count, nil
 }
 
-// perform the actual batch insert
-func (w *Writer) flush(entries []types.Entry) {
+// flush performs the actual batch insert.
+func (w *Writer) flush(entries []types.Entry) (int, error) {
 	if len(entries) == 0 {
-		return
+		return 0, nil
 	}
-	// w.writeWithMultipleInsert(entries)
-	w.writeWithCopyFrom(entries)
+	return w.writeWithFallback(entries)
+}
+
+// writeWithFallback inserts rows with the fastest method available
+// It first attempts to use writeWithCopyFrom,
+// but it falls back to writeWithMultipleInsert when we can assert the specific `duplicate key value` error
+func (w *Writer) writeWithFallback(entries []types.Entry) (int, error) {
+	copyCount, err := w.writeWithCopyFrom(entries)
+	// fallback to writeWithMultipleInsert if we have the specific `duplicate key value` error
+	if err != nil && isUniqueKeyError(err) {
+		// log.Printf("Retrying withMultipleInsert: err: %v\n", err)
+		return w.writeWithMultipleInsert(entries)
+	}
+	return copyCount, err
+}
+
+// isUniqueKeyError detects the specific error (unique key violation)
+func isUniqueKeyError(err error) bool {
+	if err != nil {
+		// Detect pgconn.PgError
+		if pge, ok := err.(*pgconn.PgError); ok {
+			// We know we have a pgconn.PgError
+			// pgconn.PgError: ERROR: duplicate key value violates unique constraint "watt_pkey" (SQLSTATE 23505)
+			if pge.Code == "23505" {
+				// log.Printf("Retrying withMultipleInsert: Code: %v TableName: %v ConstraintName: %v\n", pge.Code, pge.TableName, pge.ConstraintName)
+				return true
+			}
+		}
+		log.Printf("Unable to insert (copyFrom): %v\n", err)
+	}
+	return false
 }
 
 // writeWithCopyFrom is the fastest way to insert, but has no "ON CONFLICT (stamp) DO NOTHING" mechanism,
-// so it falls back to writeWithMultipleInsert when we can assert the specific error
-// TODO(daneroo) move error handling and fallback to flush() method, or new wrapper
-func (w *Writer) writeWithCopyFrom(entries []types.Entry) {
+func (w *Writer) writeWithCopyFrom(entries []types.Entry) (int, error) {
 	rows := [][]interface{}{}
 	for _, entry := range entries {
 		rows = append(rows, []interface{}{entry.Stamp, entry.Watt})
@@ -81,27 +111,13 @@ func (w *Writer) writeWithCopyFrom(entries []types.Entry) {
 		[]string{"stamp", "watt"},
 		pgx.CopyFromRows(rows),
 	)
-	if err != nil {
-		if pge, ok := err.(*pgconn.PgError); ok {
-			// We know we have a pgconn.PgError
-			// pgconn.PgError: ERROR: duplicate key value violates unique constraint "watt_pkey" (SQLSTATE 23505)
-			if pge.Code == "23505" {
-				// log.Printf("Retrying withMultipleInsert: Code: %v TableName: %v ConstraintName: %v\n", pge.Code, pge.TableName, pge.ConstraintName)
-				w.writeWithMultipleInsert(entries)
-				// early return
-				return
-			}
-		}
-		log.Printf("Unable to insert (copyFrom): %v\n", err)
-
-	}
-	// log last flush...
-	// if copyCount != writeBatchSize {
-	// 	log.Printf("writeWithCopyFrom inserted %d rows\n", copyCount)
-	// }
+	return int(copyCount), err
 }
 
-func (w *Writer) writeWithMultipleInsert(entries []types.Entry) {
+// Writes to postgres using a multiple value insert,
+// using `ON CONFLICT DO NOTHING`
+// The returned count is the affected rows, not necessarily the number of entries passed in.
+func (w *Writer) writeWithMultipleInsert(entries []types.Entry) (int, error) {
 
 	// log.Printf("flush: would have flushed %d entries", len(entries))
 	sql := w.makeSQL(len(entries))
@@ -113,10 +129,12 @@ func (w *Writer) writeWithMultipleInsert(entries []types.Entry) {
 	}
 
 	// commandTag, err := w.Conn.Exec(context.Background(), sql, vals...)
-	_, err := w.Conn.Exec(context.Background(), sql, vals...)
+	commandTag, err := w.Conn.Exec(context.Background(), sql, vals...)
 	if err != nil {
 		log.Printf("Unable to execute multiple insert: %v\n", err)
+		return 0, err
 	}
+	return int(commandTag.RowsAffected()), nil
 }
 
 // make multiple value insert sql statement
