baf569f479fa1c18c01ca455aad9d1e0f9d7b199
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-12-06 08:49:53 +0000

    extract logsetup

--- cmd/ipfs/ipfs.go
+++ cmd/ipfs/ipfs.go
@@ -8,29 +8,18 @@ import (
 	"time"
 
 	"github.com/daneroo/go-ted1k/ipfs"
+	"github.com/daneroo/go-ted1k/logsetup"
 	"github.com/daneroo/go-ted1k/timer"
 	"github.com/daneroo/go-ted1k/types"
 	shell "github.com/ipfs/go-ipfs-api"
 )
-
-const (
-	fmtRFC3339Millis = "2006-01-02T15:04:05.000Z07:00"
-)
-
-type logWriter struct {
-}
-
-func (writer logWriter) Write(bytes []byte) (int, error) {
-	return fmt.Print(time.Now().UTC().Format(fmtRFC3339Millis) + " - " + string(bytes))
-}
 
 // Clean everything:
 // ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm
 // ipfs repo gc
 
 func main() {
-	log.SetFlags(0)
-	log.SetOutput(new(logWriter))
+	logsetup.SetupFormat()
 	log.Printf("Starting IPFS bootstrap test\n") // TODO(daneroo): add version,buildDate
 
 	// Where your local node is running on localhost:5001
--- cmd/pump/pump.go
+++ cmd/pump/pump.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/daneroo/go-ted1k/ephemeral"
 	"github.com/daneroo/go-ted1k/ipfs"
 	"github.com/daneroo/go-ted1k/jsonl"
+	"github.com/daneroo/go-ted1k/logsetup"
 	"github.com/daneroo/go-ted1k/merge"
 	"github.com/daneroo/go-ted1k/mysql"
 	"github.com/daneroo/go-ted1k/postgres"
@@ -21,16 +21,8 @@ import (
 const (
 	myCredentials = "ted:secret@tcp(0.0.0.0:3306)/ted"
 	// pgCredentials    = "postgres://postgres:secret@127.0.0.1:5432/ted"
-	pgCredentials    = "postgres://postgres:secret@0.0.0.0:5432/ted"
-	fmtRFC3339Millis = "2006-01-02T15:04:05.000Z07:00"
+	pgCredentials = "postgres://postgres:secret@0.0.0.0:5432/ted"
 )
-
-type logWriter struct {
-}
-
-func (writer logWriter) Write(bytes []byte) (int, error) {
-	return fmt.Print(time.Now().UTC().Format(fmtRFC3339Millis) + " - " + string(bytes))
-}
 
 type entryWriter interface {
 	Write(src <-chan []types.Entry) (int, error)
@@ -40,8 +32,7 @@ type entryReader interface {
 }
 
 func main() {
-	log.SetFlags(0)
-	log.SetOutput(new(logWriter))
+	logsetup.SetupFormat()
 	log.Printf("Starting TED1K pump\n") // TODO(daneroo): add version,buildDate
 
 	tableNames := []string{"watt", "watt2"}
